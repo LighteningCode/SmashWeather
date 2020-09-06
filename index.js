67d60a3fb9762024705d5e9b01c243e3bc32e257
LigtheningCode <elvisagbesi@gmail.com>
2020-09-06 00:59:51 +0000

    Rain data states now working

--- index.js
+++ index.js
@@ -13,12 +13,23 @@ document.addEventListener('DOMContentLoaded', function () {
         windy: "./weatherStates/weather-windy.png",
     }
 
+    const DaysOfWeek = [
+        'Sunday',
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        'Saturday',
+    ];
+
     const API_KEY = '5b240d5ca7b85efd188e3bcf200f8772';
     const cityname = 'new york';
     let lat = '40.730610';
     let lon = '-73.935242';
+    let units = 'metric'
 
-    const query = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${API_KEY}`
+    const query = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&units=${units}&appid=${API_KEY}`
 
     let data = getAPIdata(encodeURI(query));
 
@@ -41,31 +52,88 @@ document.addEventListener('DOMContentLoaded', function () {
             speed: weatherData.current.wind_speed,
         }
 
-        const detailsOutput = `<span>Humidity: ${CurrentWeatherDetails.humidity}</span> <span>Temperature: ${CurrentWeatherDetails.temperature}</span> <span>Wind Speed: ${CurrentWeatherDetails.speed} miles/hour.</span>`
-        const eventStatus = `<span>Safe</span>`;
+        const detailsOutput = `<span>Humidity: ${CurrentWeatherDetails.humidity}%</span> <span>Temperature: ${CurrentWeatherDetails.temperature}°C</span> <span>Wind Speed: ${CurrentWeatherDetails.speed} miles/hour.</span>`
+
+        let eventStatus;
+
         cardTitle.innerHTML = 'Today'
         cardDescription.innerHTML = weatherData.current.weather[0].description;
         cardDetails.innerHTML = detailsOutput;
         cardEventStatus.innerHTML = eventStatus;
 
-        switch (weatherData.current.weather[0].main.toLowerCase()) {
-            case 'clouds':
+        switch (weatherData.current.weather[0].description.toLowerCase()) {
+            case 'clear sky':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.clear}" />`;
+                cardEventStatus.classList.add('safe')
+                eventStatus = `<span>Safe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'few clouds':
                 cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                cardEventStatus.classList.add('safe')
+                eventStatus = `<span>Safe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
                 break;
             case 'scattered clouds':
                 cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                cardEventStatus.classList.add('safe')
+                eventStatus = `<span>Safe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
                 break;
+            case 'broken clouds':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                cardEventStatus.classList.add('safe')
+                eventStatus = `<span>Safe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'shower rain':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy_rains_light}" />`;
+                cardEventStatus.classList.add('warn')
+                eventStatus = `<span>Warning</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'rain':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.heavy_rains}" />`;
+                cardEventStatus.classList.add('unsafe')
+                eventStatus = `<span>Unsafe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'moderate rain':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy_rains_light}" />`;
+                cardEventStatus.classList.add('warn')
+                eventStatus = `<span>Warning</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'thunderstorm':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.heavy_rains}" />`;
+                cardEventStatus.classList.add('unsafe')
+                eventStatus = `<span>Unsafe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+            case 'snow':
+                cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.snowy}" />`;
+                cardEventStatus.classList.add('safe')
+                eventStatus = `<span>Safe</span>`;
+                cardEventStatus.innerHTML = eventStatus;
+                break;
+
             default:
                 cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.clear}" />`;
                 break;
         }
 
         var cardTemplate = document.querySelector(`#weatherTemplate`);
-        
+        let dayNumber = new Date().getDay();
+        console.log(dayNumber);
+
+
+
+
+
         var newCard
-        for (let i = 0; i < weatherData.daily.length-1; i++) {
+        for (let i = 0; i < weatherData.daily.length - 1; i++) {
             newCard = cardTemplate;
-            
+
             let cardTitle = newCard.querySelector('.weather-title');
             let cardImage = newCard.querySelector('.weather-image');
             let cardDetails = newCard.querySelector('.weather-details');
@@ -79,21 +147,74 @@ document.addEventListener('DOMContentLoaded', function () {
                 speed: weatherData.daily[i].wind_speed,
             }
 
-            const detailsOutput = `<span>Humidity: ${DailyWeatherDetails.humidity}</span> <span>Temperature: ${DailyWeatherDetails.temperature}</span> <span>Wind Speed: ${DailyWeatherDetails.speed} miles/hour.</span>`
-            const eventStatus = `<span>Safe</span>`;
+            const detailsOutput = `<span>Humidity: ${DailyWeatherDetails.humidity}%</span> <span>Temperature: ${DailyWeatherDetails.temperature}°C</span> <span>Wind Speed: ${DailyWeatherDetails.speed} miles/hour.</span>`
 
-            cardTitle.innerHTML = i
+
+            dayNumber++;
+            if (dayNumber > 6) {
+                dayNumber = 0
+            }
+            cardTitle.innerHTML = DaysOfWeek[dayNumber]
             cardDescription.innerHTML = weatherData.daily[i].weather[0].description;
             cardDetails.innerHTML = detailsOutput;
-            cardEventStatus.innerHTML = eventStatus;
+            let eventStatus;
 
 
-            switch (weatherData.daily[i].weather[0].main.toLowerCase()) {
-                case 'clouds':
+
+            switch (weatherData.daily[i].weather[0].description.toLowerCase()) {
+                case 'clear sky':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.clear}" />`;
+                    cardEventStatus.classList.add('safe')
+                    eventStatus = `<span>Safe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'few clouds':
                     cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                    cardEventStatus.classList.add('safe')
+                    eventStatus = `<span>Safe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
                     break;
                 case 'scattered clouds':
                     cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                    cardEventStatus.classList.add('safe')
+                    eventStatus = `<span>Safe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'broken clouds':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy}" />`;
+                    cardEventStatus.classList.add('safe')
+                    eventStatus = `<span>Safe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'shower rain':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy_rains_light}" />`;
+                    cardEventStatus.classList.add('warn')
+                    eventStatus = `<span>Warning</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'moderate rain':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.cloudy_rains_light}" />`;
+                    cardEventStatus.classList.add('warn')
+                    eventStatus = `<span>Warning</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'rain':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.heavy_rains}" />`;
+                    cardEventStatus.classList.add('unsafe')
+                    eventStatus = `<span>Unsafe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'thunderstorm':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.heavy_rains}" />`;
+                    cardEventStatus.classList.add('unsafe')
+                    eventStatus = `<span>Unsafe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
+                    break;
+                case 'snow':
+                    cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.snowy}" />`;
+                    cardEventStatus.classList.add('safe')
+                    eventStatus = `<span>Safe</span>`;
+                    cardEventStatus.innerHTML = eventStatus;
                     break;
                 default:
                     cardImage.innerHTML = `<img class="iconImg" src="${WEATHERICON.clear}" />`;
